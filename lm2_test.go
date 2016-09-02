364e7a3244d7cb664f21908e32714abf21d230b5
Preetam Jinka <pj@preet.am>
2016-09-01 23:49:34 -0400

    faster tests, add TestWriteCloseOpen

--- lm2_test.go
+++ lm2_test.go
@@ -30,7 +30,7 @@ func TestCopy(t *testing.T) {
 	}
 	defer c.Close()
 
-	const N = 10000
+	const N = 1000
 	firstWriteStart := time.Now()
 	for i := 0; i < N; i++ {
 		key := fmt.Sprintf("%019d-%019d-%019d-%019d-%019d-%019d-%019d-%019d",
@@ -199,7 +199,7 @@ func TestWriteBatch1(t *testing.T) {
 	}
 	defer c.Close()
 
-	const N = 5000
+	const N = 500
 	for i := 0; i < N; i++ {
 		wb := NewWriteBatch()
 		key := fmt.Sprint(rand.Intn(N * 4))
@@ -264,6 +264,75 @@ func TestWriteBatch2(t *testing.T) {
 	wb = NewWriteBatch()
 	wb.Delete("key4")
 	err = c.Update(wb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	verifyOrder(t, c)
+
+	cur, err := c.NewCursor()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	i := 0
+	for cur.Next() {
+		if i == len(expected) {
+			t.Fatal("unexpected key", cur.Key())
+		}
+		if cur.Key() != expected[i][0] || cur.Value() != expected[i][1] {
+			t.Errorf("expected %v => %v, got %v => %v",
+				expected[i][0], expected[i][1], cur.Key(), cur.Value())
+		}
+		i++
+	}
+}
+
+func TestWriteCloseOpen(t *testing.T) {
+	expected := [][2]string{
+		{"key1", "1"},
+		{"key2", "2"},
+		{"key3", "1"},
+	}
+
+	c, err := NewCollection("/tmp/test_writecloseopen.lm2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wb := NewWriteBatch()
+	wb.Set("key1", "1")
+	wb.Set("key2", "1")
+	err = c.Update(wb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wb = NewWriteBatch()
+	wb.Set("key3", "1")
+	wb.Set("key2", "2")
+	err = c.Update(wb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wb = NewWriteBatch()
+	wb.Set("key4", "1")
+	err = c.Update(wb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wb = NewWriteBatch()
+	wb.Delete("key4")
+	err = c.Update(wb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c.Close()
+
+	c, err = OpenCollection("/tmp/test_writecloseopen.lm2")
 	if err != nil {
 		t.Fatal(err)
 	}
